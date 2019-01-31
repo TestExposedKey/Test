aa718fd149c6bf9999b3f99db78a5d6eb2da7b8c
TestExposedKey <47198165+TestExposedKey@users.noreply.github.com>
2019-01-30 16:48:50 -0800

    create test.java
    TEST!

---
+++ test.java
@@ -0,0 +1,8 @@
+public class test{
+  private access = "AKIAITUM5D5CARUKVHWA";
+  private secret = "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";
+  
+  public void test(){
+    //TODO: Write constructor
+  }
+}
